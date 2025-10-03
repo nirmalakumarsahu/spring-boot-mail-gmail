ba8de6a62d06f72589d0d4bf69ce9892dd1b5cf4
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-10-03 23:58:59 +0530

    Initial Commit

---
+++ src/test/java/com/sahu/mailservice/SpringBootMailGmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailGmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
